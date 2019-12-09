2d4ab5572e4e29bf96332f425f94ed12376d6db2
Alexandru Pasare <alexandru.pasare@innobyte.com>
2019-12-09 08:28:15 +0100

    use find_namespace_packages instead of find_packages for python3

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 requirements = ['scikit-learn==0.20.0', 'numpy',
                 'dependency-injector', 'pika', 'click', 'pandas']
 
 setup(name='bayes',
       version='0.1.0',
-      packages=find_packages(),
+      packages=find_namespace_packages(where='src'),
       package_dir={'': 'src'},
       install_requires=requirements,
       entry_points={
